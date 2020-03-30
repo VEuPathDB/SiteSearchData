d3129039638df6398a1fd4b69460008eec752505
steve-fischer-200 <43149795+steve-fischer-200@users.noreply.github.com>
2020-03-30 09:35:19 -0400

    delete text.py (committed by accident)

--- test.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-output = subprocess.getoutput("tail -c 1 /eupath/data/EuPathDB/siteSearchDataDumps/bld47/PlasmoDB/solr-json-batch_pathway_PlasmoDB_1583438485/batch.json")
-if output != ' ':
-  print("ERROR")
-else:
-  print("OK")
-
