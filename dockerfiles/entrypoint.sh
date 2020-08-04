ff66807f22a77d4ebaf58ab609e63acb324abd2c
Elizabeth Harper <foxcapade@gmail.com>
2020-08-04 11:49:43 -0400

    remove conifer call

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 # Populate config templates
-mkdir -p /tmp/etc
 envsubst < /tmp/site-search-config.tpl.json > /tmp/site-search-config.json
-envsubst < /tmp/conifer_site_vars.tpl.yml > /tmp/etc/conifer_site_vars.yml
 envsubst < /tmp/ldap.tpl.ora > ${TNS_ADMIN}/ldap.ora
-
-# Populate model config
-${GUS_HOME}/bin/conifer configure SiteSearchData
 
 # run CMD, or whatever is specified by run
 exec "$@"
