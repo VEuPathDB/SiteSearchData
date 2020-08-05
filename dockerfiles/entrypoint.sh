20a73185362e871bc879024b591cbdf2afb531f4
Elizabeth Harper <foxcapade@gmail.com>
2020-08-05 13:32:07 -0400

    remove oracle_home

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -2,7 +2,6 @@
 
 # Populate config templates
 envsubst < /tmp/site-search-config.tpl.json > /tmp/site-search-config.json
-envsubst < /tmp/ldap.tpl.ora > ${ORACLE_HOME}/network/admin/ldap.ora
 
 # run CMD, or whatever is specified by run
 exec "$@"
