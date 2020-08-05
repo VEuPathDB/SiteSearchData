37b580b401f5af5e1920cfda0be3063c86eb5917
Elizabeth Harper <foxcapade@gmail.com>
2020-08-05 12:43:50 -0400

    oracle_home

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Populate config templates
 envsubst < /tmp/site-search-config.tpl.json > /tmp/site-search-config.json
-envsubst < /tmp/ldap.tpl.ora > ${TNS_ADMIN}/ldap.ora
+envsubst < /tmp/ldap.tpl.ora > ${ORACLE_HOME}/network/admin/ldap.ora
 
 # run CMD, or whatever is specified by run
 exec "$@"
