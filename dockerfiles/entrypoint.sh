c783cae0dc411e4a4b939df43d9caf7533cd9d53
Bob Belnap <robert.belnap@uga.edu>
2020-08-11 13:29:41 -0400

    add simple templates for model-config in entrypoint and add presenter_update script

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 # Populate config templates
-envsubst < /tmp/site-search-config.tpl.json > /tmp/site-search-config.json
+# this should be a loop, or something more sane, but we're dealing with sh here.
+
+if [ ! -f ${GUS_HOME}/config/SiteSearchData/model-config.xml ];
+then
+    envsubst < ${GUS_HOME}/config/SiteSearchData/model-config.xml.tmpl > ${GUS_HOME}/config/SiteSearchData/model-config.xml 
+fi
+
+if [ ! -f ${GUS_HOME}/config/SiteSearchData/model.prop ];
+then
+    envsubst < ${GUS_HOME}/config/SiteSearchData/model.prop.tmpl > ${GUS_HOME}/config/SiteSearchData/model.prop
+fi
+
+#envsubst < /tmp/site-search-config.tpl.json > /tmp/site-search-config.json
 
 # run CMD, or whatever is specified by run
 exec "$@"
