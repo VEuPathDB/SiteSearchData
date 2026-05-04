f7fc6a9ebb2bafbbfe156c5e369f616a16ae7bcb
Mustafa Nural <mvnural@gmail.com>
2026-05-04 11:15:17 -0400

    Add runWebsiteBuild.sh script.
    This will be used by jenkins to launch nextflow websiteBuildFlow during site builds.

---
+++ nextflow/runWebsiteBuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+EFFECTIVE_COHORT="${COHORT}"
+if [[ "${PROJECT_ID}" == "UniDB" ]]; then
+  EFFECTIVE_COHORT="Portal"
+fi
+
+nextflow run websiteBuildFlow.nf \
+  --containerImage "docker.io/veupathdb/site-search-data:$IMAGE_BRANCH" \
+  --envFile "$ENV_FILE" \
+  --podmanRunOptions "--sysctl net.ipv6.conf.all.disable_ipv6=1 --network=pasta:\"--map-host-loopback=169.254.1.2\" --add-host=${SOLR_DOMAIN}:169.254.1.2 --add-host=${SITE_DOMAIN}:169.254.1.2" \
+  --outputDir "$OUTPUT_DIR/ssnextflow" \
+  --siteBaseUrl "$SITE_BASE_URL" \
+  --cohort "$EFFECTIVE_COHORT" \
+  --projectId "$PROJECT_ID" \
+  --solrUrl "$SOLR_URL"
+
