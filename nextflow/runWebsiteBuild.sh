6145bba3d89e2b0f55dd094eed6dc7c357f21084
Mustafa Nural <mvnural@gmail.com>
2026-05-06 16:16:22 -0400

    restore site domain mapping and add confined flag.

--- nextflow/runWebsiteBuild.sh
+++ nextflow/runWebsiteBuild.sh
@@ -11,10 +11,15 @@ if [[ -n "${CLEANUP:-}" ]]; then
   CLEANUP_ARG="--cleanupOnExit $CLEANUP"
 fi
 
+CONFINED_ARG=""
+if [[ "${UNCONFINED:-true}" == "true"  ]]; then
+  CONFINED_ARG="--security-opt label=disable"
+fi
+
 nextflow run websiteBuildFlow.nf \
   --containerImage "docker.io/veupathdb/site-search-data:$IMAGE_BRANCH" \
   --envFile "$ENV_FILE" \
-  --podmanRunOptions "--sysctl net.ipv6.conf.all.disable_ipv6=1 --network=pasta:\"--map-host-loopback=169.254.1.2\" --add-host=${SOLR_DOMAIN}:169.254.1.2  --security-opt label=disable" \
+  --podmanRunOptions "--sysctl net.ipv6.conf.all.disable_ipv6=1 --network=pasta:\"--map-host-loopback=169.254.1.2\" --add-host=${SOLR_DOMAIN}:169.254.1.2 --add-host=${SITE_DOMAIN}:169.254.1.2 ${CONFINED_ARG}" \
   --outputDir "$OUTPUT_DIR/ssnextflow" \
   --siteBaseUrl "$SITE_BASE_URL" \
   --cohort "$EFFECTIVE_COHORT" \
